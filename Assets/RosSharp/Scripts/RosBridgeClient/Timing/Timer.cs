59df7218fcaf8d03870173cee6ef248475854e96
RyodoTanaka <groadpg@gmail.com>
2020-05-07 22:52:32 +0900

    First commit

---
+++ Assets/RosSharp/Scripts/RosBridgeClient/Timing/Timer.cs
@@ -0,0 +1,44 @@
+﻿/*
+© Siemens AG, 2017-2019
+Author: Dr. Martin Bischoff (martin.bischoff@siemens.com)
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+<http://www.apache.org/licenses/LICENSE-2.0>.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Adding Timestamp switching
+// Shimadzu corp , 2019, Akira NODA (a-noda@shimadzu.co.jp / you.akira.noda@gmail.com)
+
+// Removing MonoBehaviour inheritance
+// Siemens AG , 2019, Berkay Alp Cakal (berkay_alp.cakal.ct@siemens.com) 
+
+using System;
+using RosSharp.RosBridgeClient.MessageTypes.Std;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class Timer
+    {
+        public static DateTime UNIX_EPOCH = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+
+        public virtual MessageTypes.Std.Time Now()
+        {
+            TimeSpan timeSpan = DateTime.Now.ToUniversalTime() - UNIX_EPOCH;
+
+            double msecs = timeSpan.TotalMilliseconds;
+            uint sec = (uint)(msecs / 1000);
+
+            return new MessageTypes.Std.Time
+            {
+                secs = sec,
+                nsecs = (uint)((msecs / 1000 - sec) * 1e+9)
+            };
+        }
+    }
+}
---
+++ Assets/Scripts/VelodyneLidarUnity/Lidar.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lidar : MonoBehaviour {
+
+    public float maxAngle = 10;
+    public float minAngle = -10;
+    public int numberOfLayers = 16;
+    public int numberOfIncrements = 360;
+    public float maxRange = 100f;
+
+    float vertIncrement;
+    float azimutIncrAngle;
+
+    [HideInInspector]
+    public float[] distances;
+    public float[] azimuts;
+
+
+    // Use this for initialization
+    void Start () {
+        distances = new float[numberOfLayers* numberOfIncrements];
+        azimuts = new float[numberOfIncrements];
+        vertIncrement = (float)(maxAngle - minAngle) / (float)(numberOfLayers - 1);
+        azimutIncrAngle = (float)(360.0f / numberOfIncrements);
+    }
+
+// Update is called once per frame
+void FixedUpdate () {
+        Vector3 fwd = new Vector3(0, 0, 1);
+        Vector3 dir;
+        RaycastHit hit;
+        int indx = 0;
+        float angle;
+
+        //azimut angles
+        for (int incr = 0; incr < numberOfIncrements; incr++)
+        {
+            for (int layer = 0; layer < numberOfLayers; layer++)
+            {
+                //print("incr "+ incr +" layer "+layer+"\n");
+                indx = layer + incr * numberOfLayers;
+                angle = minAngle + (float)layer * vertIncrement;
+                azimuts[incr] = incr * azimutIncrAngle;
+                dir = transform.rotation * Quaternion.Euler(-angle, azimuts[incr], 0)*fwd;
+                // print("idx "+ indx +" angle " + angle + "  azimut " + azimut + " quats " + Quaternion.Euler(-angle, azimut, 0) + " dir " + dir+ " fwd " + fwd+"\n");
+
+                //Debug.DrawRay(transform.position, dir * 100.0f, Color.green);
+                if (Physics.Raycast(transform.position, dir, out hit, maxRange))
+                {
+                    distances[indx] = (float)hit.distance;
+                }
+                else
+                {
+                    distances[indx] = maxRange;
+                }
+            }
+        }
+
+    }
+}
