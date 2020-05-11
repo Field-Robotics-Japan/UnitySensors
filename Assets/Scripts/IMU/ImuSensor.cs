c49969fab239410b4a1c0ba5977380ad93f5c91a
calm0815(kuwano) <kuwano.tomohiro455@mail.kyutech.jp>
2020-05-11 09:29:59 +0900

    Development of the IMU was completed.

--- Assets/Scripts/IMU/ImuSensor.cs
+++ Assets/Scripts/IMU/ImuSensor.cs
@@ -5,28 +5,23 @@ namespace RosSharp.RosBridgeClient
 {
     public class ImuSensor : UnityPublisher<MessageTypes.Sensor.Imu>
     {
-        public Transform PublishedTransform;
+        private Transform trans;
+        private Rigidbody rb;
         public string FrameId = "Unity";
         private MessageTypes.Sensor.Imu message;
-
         private float deltaTime;
 
-        ///  values
-        private Vector3 _AngVel = Vector3.zero;
-        private Vector3 _LinAcc = Vector3.zero;
-
         /// previous value
-        private Vector3 _prePosition = Vector3.zero;
-        private Vector3 _preRotate = Vector3.zero;
-        private Vector3 _preAngVel = Vector3.zero;
-        private Vector3 _preLinVel = Vector3.zero;
-        private Vector3 _preLinAcc = Vector3.zero;
+        private Vector3 LastVelocity = Vector3.zero;
 
         protected override void Start()
         {
             base.Start();
             InitializeMessage();
+            rb = GetComponent<Rigidbody>();
+            trans = GetComponent<Transform>();
         }
+
         private void FixedUpdate()
         {
             UpdateMessage();
@@ -41,53 +36,47 @@ namespace RosSharp.RosBridgeClient
         private void UpdateMessage()
         {
             message.header.Update();
-            Vector3 position = PublishedTransform.position;
-            Quaternion quaternion = PublishedTransform.rotation;
+            Transform transform = trans;
+            Rigidbody rigidbody = rb;
             deltaTime = Time.deltaTime;
-            message.orientation = GetGeometryQuaternion(quaternion);
-            message.angular_velocity = GetAngularVelocity(quaternion.eulerAngles);
-            message.linear_acceleration = GetLinearAcceleration(position, quaternion.eulerAngles);
+            message.orientation = GetGeometryQuaternion(transform);
+            message.angular_velocity = GetAngularVelocity(rigidbody);
+            message.linear_acceleration = GetLinearAcceleration(transform, rigidbody);
             Publish(message);
         }
 
-        private MessageTypes.Geometry.Quaternion GetGeometryQuaternion(Quaternion quaternion)
+        private MessageTypes.Geometry.Quaternion GetGeometryQuaternion(Transform transform)
         {
             MessageTypes.Geometry.Quaternion geometryQuaternion = new MessageTypes.Geometry.Quaternion();
-            geometryQuaternion.x = quaternion.Unity2Ros().x;
-            geometryQuaternion.y = quaternion.Unity2Ros().y;
-            geometryQuaternion.z = quaternion.Unity2Ros().z;
-            geometryQuaternion.w = quaternion.Unity2Ros().w;
+            geometryQuaternion.x = transform.rotation.Unity2Ros().x;
+            geometryQuaternion.y = transform.rotation.Unity2Ros().y;
+            geometryQuaternion.z = transform.rotation.Unity2Ros().z;
+            geometryQuaternion.w = transform.rotation.Unity2Ros().w;
             return geometryQuaternion;
         }
 
-        private MessageTypes.Geometry.Vector3 GetAngularVelocity(Vector3 rotation)
+        private MessageTypes.Geometry.Vector3 GetAngularVelocity(Rigidbody rigidbody)
         {
-            _AngVel = (rotation - _preRotate) / deltaTime;
-            _preRotate = rotation;
-
             MessageTypes.Geometry.Vector3 result = new MessageTypes.Geometry.Vector3();
-            result.x = _AngVel.Unity2Ros().x;
-            result.y = _AngVel.Unity2Ros().y;
-            result.z = _AngVel.Unity2Ros().z;
+            result.x = rigidbody.angularVelocity.Unity2Ros().x;
+            result.y = rigidbody.angularVelocity.Unity2Ros().y;
+            result.z = rigidbody.angularVelocity.Unity2Ros().z;
             return result;
         }
 
-        private MessageTypes.Geometry.Vector3 GetLinearAcceleration(Vector3 pose, Vector3 rotation)
+        private MessageTypes.Geometry.Vector3 GetLinearAcceleration(Transform transform, Rigidbody rigidbody)
         {
-            // move
-            Vector3 linVel = (pose - _prePosition) / deltaTime;
-            _LinAcc = (linVel - _preLinVel) / deltaTime;
-            _preLinVel = linVel;
-            _preLinAcc = _LinAcc;
-
-            //gravity
-
-            /* not yet */
+            // move element
+            Vector3 localLinearVelocity = transform.InverseTransformDirection(rigidbody.velocity);
+            Vector3 acceleration = ( localLinearVelocity - LastVelocity ) / deltaTime;
+            LastVelocity = localLinearVelocity;
+            //gravity element
+            acceleration += transform.InverseTransformDirection(Physics.gravity);
 
             MessageTypes.Geometry.Vector3 result = new MessageTypes.Geometry.Vector3();
-            result.x = _LinAcc.Unity2Ros().x;
-            result.y = _LinAcc.Unity2Ros().y;
-            result.z = _LinAcc.Unity2Ros().z;
+            result.x = acceleration.Unity2Ros().x;
+            result.y = acceleration.Unity2Ros().y;
+            result.z = acceleration.Unity2Ros().z;
             return result;
         }
     }
