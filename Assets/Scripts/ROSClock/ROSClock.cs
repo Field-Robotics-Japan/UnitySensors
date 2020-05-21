bef997929f684b182bc1b42796fda805217df5b3
RyodoTanaka <groadpg@gmail.com>
2020-05-21 16:35:33 +0900

    Add Clock publisher

---
+++ Assets/Scripts/ROSClock/ROSClock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class ROSClock : UnityPublisher<MessageTypes.Rosgraph.Clock>
+    {
+        private MessageTypes.Rosgraph.Clock message;
+
+        protected override void Start()
+        {
+            base.Start();
+            message = new MessageTypes.Rosgraph.Clock();
+        }
+
+        private void FixedUpdate()
+        {
+            float sim_time = Time.time;
+            uint secs = (uint)sim_time;
+            uint nsecs = (uint)((sim_time % 1) * 1e6);
+            message.clock.secs = secs;
+            message.clock.nsecs = nsecs;
+            Publish(message);
+        }
+    }
+}
