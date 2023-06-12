573e32a434c6e39b7655f12bf9d723f14f26b6fa
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-06-12 18:03:33 +0900

    Create GaussianNoise.cs

---
+++ Assets/UnitySensors/Runtime/Scripts/Utils/Noise/GaussianNoise.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Random = System.Random;
+
+namespace UnitySensors
+{
+    public class GaussianNoise
+    {
+        private Random _random;
+
+        public GaussianNoise()
+        {
+            _random = new Random(Environment.TickCount);
+        }
+
+        public GaussianNoise(int seed)
+        {
+            _random = new Random(seed);
+        }
+
+        public void Init(int seed)
+        {
+            _random = new Random(seed);
+        }
+
+        public double GetNoise(double sigma = 1.0d)
+        {
+            double rand = _random.NextDouble();
+            double normrand = Math.Sqrt(-2.0d * Math.Log(0.0d)) * Math.Cos(2.0d * Math.PI * rand);
+            return normrand;
+        }
+    }
+}
