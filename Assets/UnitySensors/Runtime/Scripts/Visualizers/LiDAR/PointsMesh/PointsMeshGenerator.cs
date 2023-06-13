1d186c272741fdf0e80984ec0f97a8f69f74013e
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-06-13 17:04:19 +0900

    Create VelodynePointsMesh

--- Assets/UnitySensors/Runtime/Scripts/Visualizers/LiDAR/PointsMesh/PointsMeshGenerator.cs
+++ Assets/UnitySensors/Runtime/Scripts/Visualizers/LiDAR/PointsMesh/PointsMeshGenerator.cs
@@ -4,30 +4,65 @@ using UnityEngine;
 
 namespace UnitySensors.Visualization
 {
-    [System.Serializable]
-    public class PointsMeshGenerator
+    [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+    public class PointsMeshGenerator : MonoBehaviour
     {
         [SerializeField]
+        private float _frequency = 10;
+
+        [SerializeField]
+        private Material _material;
+
         private MeshFilter _meshFilter;
+        private MeshRenderer _meshRenderer;
 
         private Mesh _mesh;
         private int[] _indices;
 
-        public void Init(int pointsNum)
+        private float _time_last;
+        private float _frequency_inv;
+
+        private void Awake()
         {
-            _indices = new int[pointsNum];
-            for(int i = 0; i < pointsNum; i++)
-            {
-                _indices[i] = i;
-            }
+            _meshFilter = GetComponent<MeshFilter>();
+            _meshRenderer = GetComponent<MeshRenderer>();
         }
 
-        public void Generate(Vector3[] points)
+        private void Start()
         {
+            if (_material) _meshRenderer.material = _material;
+
+            _frequency_inv = 1.0f / _frequency;
+        }
+
+        private void Update()
+        {
+            float time_now = Time.time;
+            if (time_now - _time_last < _frequency_inv) return;
+
+            Generate();
+
+            _time_last = time_now;
+        }
+
+        protected virtual void Generate()
+        {
+
+        }
+
+        protected void GenerateMesh(Vector3[] points)
+        {
+            if (_indices == null || _indices.Length != points.Length) GenerateIndices(points.Length);
             _mesh = new Mesh();
-            _meshFilter.mesh = _mesh;
             _mesh.vertices = points;
             _mesh.SetIndices(_indices, MeshTopology.Points, 0);
+            _meshFilter.mesh = _mesh;
+        }
+
+        private void GenerateIndices(int pointsNum)
+        {
+            _indices = new int[pointsNum];
+            for (int i = 0; i < pointsNum; i++) _indices[i] = i;
         }
     }
 }
