3489ddf55cddfb865419e26765c4f1b6f4ae7ff5
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-06-13 16:25:22 +0900

    Add noise to velodyne

--- Assets/UnitySensors/Runtime/Scripts/Sensors/LiDAR/Velodyne/VelodyneSensor.cs
+++ Assets/UnitySensors/Runtime/Scripts/Sensors/LiDAR/Velodyne/VelodyneSensor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,18 +8,22 @@ using Unity.Collections;
 using UnityEngine.Jobs;
 using Unity.Jobs;
 
+using Random = Unity.Mathematics.Random;
+
 namespace UnitySensors
 {
     public class VelodyneSensor : Sensor
     {
         [SerializeField]
         private RotatingLiDARScanPattern _scanPattern;
-
+        [SerializeField]
+        private float _gaussianNoiseSigma = 0.0f;
         [SerializeField]
         private bool _safeDisposal = false;
 
         private JobHandle _handle;
         private UpdateRaycastCommandsJob _updateRaycastCommandsJob;
+        private UpdateGaussianNoisesJob _updateGaussianNoisesJob;
         private RaycastHitsToPointsJob _raycastHitsToPointsJob;
 
         private TransformAccessArray _transformAccessArray;
@@ -26,8 +31,11 @@ namespace UnitySensors
         private NativeArray<Vector3> _directions;
         private NativeArray<RaycastCommand> _raycastCommands;
         private NativeArray<RaycastHit> _raycastHits;
+        private Random _random;
+        private NativeArray<float> _noises;
         public NativeArray<Vector3> points;
 
+        private uint _randomSeed;
         private int _pointsNum;
         public uint pointsNum { get => (uint)_pointsNum; }
 
@@ -66,16 +74,29 @@ namespace UnitySensors
             _raycastCommands = new NativeArray<RaycastCommand>(_pointsNum, Allocator.Persistent);
             _raycastHits = new NativeArray<RaycastHit>(_pointsNum, Allocator.Persistent);
 
+            _randomSeed = (uint)Environment.TickCount;
+            _random = new Random(_randomSeed);
+
+            _noises = new NativeArray<float>(_pointsNum, Allocator.Persistent);
+
             _updateRaycastCommandsJob = new UpdateRaycastCommandsJob()
             {
                 directions = _directions,
                 raycastCommands = _raycastCommands
             };
 
+            _updateGaussianNoisesJob = new UpdateGaussianNoisesJob()
+            {
+                sigma = _gaussianNoiseSigma,
+                random = _random,
+                noises = _noises
+            };
+
             _raycastHitsToPointsJob = new RaycastHitsToPointsJob()
             {
                 directions = _directions,
                 raycastHits = _raycastHits,
+                noises = _noises,
                 points = points
             };
         }
@@ -84,8 +105,12 @@ namespace UnitySensors
         {
             _handle.Complete();
 
+            if(_randomSeed++ == 0) _randomSeed = 1;
+            _updateGaussianNoisesJob.random.InitState(_randomSeed);
+
             JobHandle updateRaycastCommandsJobHandle = _updateRaycastCommandsJob.Schedule(_transformAccessArray);
-            JobHandle raycastJobHandle = RaycastCommand.ScheduleBatch(_raycastCommands, _raycastHits, _pointsNum, updateRaycastCommandsJobHandle);
+            JobHandle updateGaussianNoisesJobHandle = _updateGaussianNoisesJob.Schedule(_pointsNum, 1, updateRaycastCommandsJobHandle);
+            JobHandle raycastJobHandle = RaycastCommand.ScheduleBatch(_raycastCommands, _raycastHits, _pointsNum, updateGaussianNoisesJobHandle);
             _handle = _raycastHitsToPointsJob.Schedule(_pointsNum, 1, raycastJobHandle);
 
             JobHandle.ScheduleBatchedJobs();
@@ -101,6 +126,7 @@ namespace UnitySensors
             _handle.Complete();
             if(_safeDisposal)
                 _transformAccessArray.Dispose();
+            _noises.Dispose();
             _directions.Dispose();
             _raycastCommands.Dispose();
             _raycastHits.Dispose();
@@ -122,18 +148,38 @@ namespace UnitySensors
         }
 
         [BurstCompile]
+        private struct UpdateGaussianNoisesJob : IJobParallelFor
+        {
+            public float sigma;
+            public Random random;
+            public NativeArray<float> noises;
+
+            public void Execute(int index)
+            {
+                var rand2 = random.NextFloat();
+                var rand3 = random.NextFloat();
+                float normrand =
+                    (float)Math.Sqrt(-2.0f * Math.Log(rand2)) *
+                    (float)Math.Cos(2.0f * Math.PI * rand3);
+                noises[index] = sigma * normrand;
+            }
+        }
+
+        [BurstCompile]
         private struct RaycastHitsToPointsJob : IJobParallelFor
         {
             [ReadOnly]
             public NativeArray<Vector3> directions;
             [ReadOnly]
             public NativeArray<RaycastHit> raycastHits;
+            [ReadOnly]
+            public NativeArray<float> noises;
 
             public NativeArray<Vector3> points;
 
             public void Execute(int index)
             {
-                points[index] = directions[index] * raycastHits[index].distance;
+                points[index] = directions[index] * (raycastHits[index].distance + noises[index]);
             }
         }
     }
