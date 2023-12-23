b47f64476a7edb4faf0fc4bff31615d00b5872a6
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-12-23 22:18:36 +0900

    Update Color2Depth.cs

--- Assets/UnitySensors/Runtime/Scripts/Utils/Camera/Color2Depth.cs
+++ Assets/UnitySensors/Runtime/Scripts/Utils/Camera/Color2Depth.cs
@@ -7,9 +7,17 @@ namespace UnitySensors.Utils.Camera
     {
         private Material _mat;
 
-        private void Awake()
+        [System.NonSerialized]
+        public float y_min = 0.0f;
+        [System.NonSerialized]
+        public float y_max = 1.0f;
+
+        private void Start()
         {
             _mat = new Material(Shader.Find("UnitySensors/Color2Depth"));
+            _mat.SetFloat("_F", GetComponent<UnityEngine.Camera>().farClipPlane);
+            _mat.SetFloat("_Y_MIN", y_min);
+            _mat.SetFloat("_Y_MAX", y_max);
         }
 
         private void OnRenderImage(RenderTexture source, RenderTexture dest)
