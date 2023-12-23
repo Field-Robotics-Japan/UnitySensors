d37557ae21e6c0f3b7458637896cc0bb2ea7f9b5
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-12-23 22:18:58 +0900

    Update ITextureToPointsJob.cs

--- Assets/UnitySensors/Runtime/Scripts/Sensors/LiDAR/DepthBufferLiDAR/ITextureToPointsJob.cs
+++ Assets/UnitySensors/Runtime/Scripts/Sensors/LiDAR/DepthBufferLiDAR/ITextureToPointsJob.cs
@@ -14,6 +14,7 @@ namespace UnitySensors.Sensor.LiDAR
     {
         public float near;
         public float far;
+        public int indexOffset;
 
         [ReadOnly]
         public NativeArray<float3> directions;
@@ -28,13 +29,12 @@ namespace UnitySensors.Sensor.LiDAR
 
         public void Execute(int index)
         {
-            int pixelIndex = pixelIndices[index];
+            int pixelIndex = pixelIndices[index + indexOffset];
             float distance = Mathf.Clamp01(1.0f - pixels.AsReadOnly()[pixelIndex].r) * far;
             distance = (near < distance && distance < far) ? distance/* + noises[index]*/ : 0;
-            distance = 10.0f;
             PointXYZI point = new PointXYZI()
             {
-                position = directions[index] * distance
+                position = directions[index + indexOffset] * distance
             };
             
             points[index] = point;
