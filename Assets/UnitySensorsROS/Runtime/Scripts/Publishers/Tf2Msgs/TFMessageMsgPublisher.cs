c5b6b12385db031a65cf90234d72f34127ba685f
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2024-02-19 02:20:47 +0900

    Update tf scripts

---
+++ Assets/UnitySensorsROS/Runtime/Scripts/Publishers/Tf2Msgs/TFMessageMsgPublisher.cs
@@ -0,0 +1,9 @@
+using RosMessageTypes.Tf2;
+using UnitySensors.ROS.Serializer.Tf2;
+
+namespace UnitySensors.ROS.Publisher.Tf2
+{
+    public class TFMessageMsgPublisher : RosMsgPublisher<TFMessageMsgSerializer, TFMessageMsg>
+    {
+    }
+}
