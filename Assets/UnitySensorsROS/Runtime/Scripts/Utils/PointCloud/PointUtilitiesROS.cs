5fe9b412076fccfe0e588c7893107b664f276426
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2024-02-19 02:18:45 +0900

    Create PointUtilitites

---
+++ Assets/UnitySensorsROS/Runtime/Scripts/Utils/PointCloud/PointUtilitiesROS.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+using RosMessageTypes.Sensor;
+
+using UnitySensors.DataType.Sensor.PointCloud;
+
+namespace UnitySensors.ROS.Utils.PointCloud
+{
+    public static class PointUtilitiesROS
+    {
+        public readonly static ReadOnlyDictionary<Type, PointFieldMsg[]> pointFields = new ReadOnlyDictionary<Type, PointFieldMsg[]>(new Dictionary<Type, PointFieldMsg[]>
+        {
+            {
+                typeof(PointXYZ), 
+                new PointFieldMsg[]
+                {
+                    new PointFieldMsg("y", 0, 7, 1),
+                    new PointFieldMsg("z", 4, 7, 1),
+                    new PointFieldMsg("x", 8, 7, 1),
+                }
+            },
+            {
+                typeof(PointXYZI),
+                new PointFieldMsg[]
+                {
+                    new PointFieldMsg("y", 0, 7, 1),
+                    new PointFieldMsg("z", 4, 7, 1),
+                    new PointFieldMsg("x", 8, 7, 1),
+                    new PointFieldMsg("i", 12, 7, 1),
+                }
+            },
+            {
+                typeof(PointXYZRGB),
+                new PointFieldMsg[]
+                {
+                    new PointFieldMsg("y", 0, 7, 1),
+                    new PointFieldMsg("z", 4, 7, 1),
+                    new PointFieldMsg("x", 8, 7, 1),
+                    new PointFieldMsg("rgba", 12, 6, 1)
+                }
+            }
+        });
+    }
+}
