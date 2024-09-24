d1da2d0f1f06f5c27d9f707e68bca5f5b3fc31b5
Alexandre Foucher <alexandrefoucher01@gmail.com>
2024-09-24 18:04:42 +0200

    Switch compressed image to raw image

--- Assets/UnitySensorsROS/Runtime/Scripts/Serializers/Image/ImageMsgSerializer.cs
+++ Assets/UnitySensorsROS/Runtime/Scripts/Serializers/Image/ImageMsgSerializer.cs
@@ -7,25 +7,27 @@ using UnitySensors.Sensor;
 namespace UnitySensors.ROS.Serializer.Image
 {
     [System.Serializable]
-    public class ImageMsgSerializer<T> : RosMsgSerializer<T, CompressedImageMsg> where T : UnitySensor, ITextureInterface
+    public class ImageMsgSerializer<T> : RosMsgSerializer<T, ImageMsg> where T : UnitySensor, ITextureInterface
     {
         [SerializeField]
         private HeaderSerializer _header;
-        [SerializeField, Range(1, 100)]
-        private int quality = 75;
 
         public override void Init(T sensor)
         {
             base.Init(sensor);
             _header.Init(sensor);
 
-            _msg.format = "jpeg";
+            _msg.height       = (uint)sensor.height;
+            _msg.width        = (uint)sensor.width;
+            _msg.encoding     = sensor.encoding;
+            _msg.is_bigendian = 0;
+            _msg.step         = 1 * 3 * _msg.width;
         }
 
-        public override CompressedImageMsg Serialize()
+        public override ImageMsg Serialize()
         {
             _msg.header = _header.Serialize();
-            _msg.data = sensor.texture.EncodeToJPG(quality);
+            _msg.data = sensor.data;
             return _msg;
         }
     }
